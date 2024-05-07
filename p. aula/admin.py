f5800196da8109f00c94c75e404fa0dd481b5869
Misael14199 <160240517+Misael14199@users.noreply.github.com>
2024-05-06 22:33:49 -0500

    Add files via upload

--- p. aula/admin.py
+++ p. aula/admin.py
@@ -3,31 +3,31 @@ from PIL import Image, ImageTk
 
 
 #ventana principal
-ventanai = tk.Tk()
-pathi = Image.open("inicio-img.png")
-iconoi = ImageTk.PhotoImage(pathi)
-ventanai.iconphoto(True, iconoi)
-ventanai.title("Inicio de sesion") 
-ventanai.geometry(f"1024x650")
-ventanai.resizable(False, False) 
-ventanai.config(bg="black")
+ventanaa = tk.Tk()
+patha = Image.open("admin5.png")
+iconoa = ImageTk.PhotoImage(patha)
+ventanaa.iconphoto(True, iconoa)
+ventanaa.title("Configuraciones de administrador") 
+ventanaa.geometry(f"1050x650")
+ventanaa.resizable(False, False) 
+ventanaa.config(bg="black")
 
 
 #titulos guias
-titulo_registror = tk.Label(ventanai, 
-                           text="INICIO DE SESION", 
+titulo_registror = tk.Label(ventanaa, 
+                           text="SESION DE ADMINISTRADOR", 
                            font=("Cooper Black", 30), background="black", 
                            foreground="white")
 titulo_registror.place(x = 400, y = 100, height=28)
 
-nombre_usuarioi = tk.Label(ventanai, 
+nombre_usuarioi = tk.Label(ventanaa, 
                   text="Ingrese su nombre de usuario", 
                   font=("Cooper Black", 15), 
                   background="black", 
                   foreground="white")
 nombre_usuarioi. place(x = 400, y = 170)
 
-clavei = tk.Label(ventanai, 
+clavei = tk.Label(ventanaa, 
                     text="Ingrese su clave", 
                     font=("Cooper Black", 15), 
                     background="black", 
@@ -39,13 +39,13 @@ clavei.place(x = 400, y = 270)
 
 
 #ingreso de datos por el usuario
-nombre_usuarioi1 = tk.Entry(ventanai, width=25, font=20, bg="black", foreground="white", border=0)
+nombre_usuarioi1 = tk.Entry(ventanaa, width=25, font=20, bg="black", foreground="white", border=0)
 nombre_usuarioi1.place(x = 400, y = 200, height=22)
-clavei1 = tk.Entry(ventanai, width=25, font=20, bg="black", foreground="white", border=0)
+clavei1 = tk.Entry(ventanaa, width=25, font=20, bg="black", foreground="white", border=0)
 clavei1.place(x = 400, y = 300, height=22)
 
-tk.Frame(ventanai, width=320, height=2, bg="white").place(x=400, y=222)
-tk.Frame(ventanai, width=320, height=2, bg="white").place(x=400, y=322)
+tk.Frame(ventanaa, width=320, height=2, bg="white").place(x=400, y=222)
+tk.Frame(ventanaa, width=320, height=2, bg="white").place(x=400, y=322)
 
 
 
@@ -55,14 +55,14 @@ def inicio():
     nombre_usuarioi2 = nombre_usuarioi1.get()
     clavei2 = clavei1.get()
 
-botoni = tk.Button(ventanai, text = "Iniciar Sesion", command = inicio, font=("Cooper Black", 18), border=5)
+botoni = tk.Button(ventanaa, text = "Iniciar Sesion", command = inicio, font=30, borderwidth=5)
 botoni.place(x = 540, y = 370) 
 
 
-frame_logor = tk.Frame(ventanai, bd=0, width=320, relief=tk.SOLID, padx=30, pady=210, bg="black")
+frame_logor = tk.Frame(ventanaa, bd=0, width=320, relief=tk.SOLID, padx=30, pady=210, bg="black")
 frame_logor.pack(side=tk.LEFT, expand=tk.NO, fill=tk.BOTH)
-labelr = tk.Label(frame_logor, image=iconoi, bg="black")
+labelr = tk.Label(frame_logor, image=iconoa, bg="white")
 labelr.place(x=50, y=0, relwidth=1, relheight=1)
 
 
-ventanai.mainloop()
+ventanaa.mainloop()
--- p. aula/registro.py
+++ p. aula/registro.py
@@ -22,66 +22,64 @@ titulo_registror = tk.Label(ventanar,
                            text="REGISTRO DE USUARIO", 
                            font=("Cooper Black", 30), background="black", 
                            foreground="white")
-titulo_registror.place(x = 400, y = 170, height=28)
+titulo_registror.place(x = 400, y = 100, height=28)
 
 nombrer = tk.Label(ventanar, 
                   text="Ingrese su nombre", 
                   font=("Cooper Black", 15), 
                   background="black", 
                   foreground="white")
-nombrer. place(x = 400, y = 216)
+nombrer. place(x = 400, y = 166)
 
 apellidor = tk.Label(ventanar, 
                     text="Ingrese su apellido", 
                     font=("Cooper Black", 15), 
                     background="black", 
                     foreground="white")
-apellidor.place(x = 400, y = 275)
+apellidor.place(x = 400, y = 225)
 
 correor = tk.Label(ventanar, 
                   text="Ingrese su correo", 
                   font=("Cooper Black", 15), 
                   background="black", 
                   foreground="white")
-correor.place(x = 400, y = 335)
+correor.place(x = 400, y = 285)
 
 nombre_de_usuarior = tk.Label(ventanar, 
                              text="Ingrese su nombre de usuario", 
                              font=("Cooper Black", 15), 
                              background="black", 
                              foreground="white")
-nombre_de_usuarior.place(x = 400, y = 396)
+nombre_de_usuarior.place(x = 400, y = 346)
 
 claver = tk.Label(ventanar, 
                  text="Ingrese su contraseña a usar", 
                  font=("Cooper Black", 15), 
                  background="black", 
                  foreground="white")
-claver.place(x = 400, y = 456)
+claver.place(x = 400, y = 406)
 
 
 
 
 #ingreso de datos por el usuario
-
-
 nombre1r = tk.Entry(ventanar, width=25, font=20, bg="black", foreground="white", border=0)
-nombre1r.place(x = 400, y = 244, height=22)
+nombre1r.place(x = 400, y = 194, height=22)
 apellido1r = tk.Entry(ventanar, width=25, font=20, bg="black", foreground="white", border=0)
-apellido1r.place(x = 400, y = 303, height=22)
+apellido1r.place(x = 400, y = 253, height=22)
 correo1r = tk.Entry(ventanar, width=35, font=20, bg="black", foreground="white", border=0)
-correo1r.place(x = 400, y = 363, height=22)
+correo1r.place(x = 400, y = 313, height=22)
 nombre_de_usuario1r = tk.Entry(ventanar, width=25, font=20, bg="black", foreground="white", border=0)
-nombre_de_usuario1r.place(x = 400, y = 424, height=22)
+nombre_de_usuario1r.place(x = 400, y = 374, height=22)
 clave1r = tk.Entry(ventanar, width=30, font=20, bg="black", foreground="white", border=0)
-clave1r.place(x = 400, y = 484, height=22)
+clave1r.place(x = 400, y = 434, height=22)
 
 
-tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=265)
-tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=325)
-tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=384)
-tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=445)
-tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=505)
+tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=215)
+tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=275)
+tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=334)
+tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=395)
+tk.Frame(ventanar, width=320, height=2, bg="white").place(x=400, y=455)
 
 
 
@@ -95,14 +93,14 @@ def registro():
     clave2 = clave1r.get()
 
 botonr = tk.Button(ventanar, text = "Registrarse", command = registro, font=("Cooper Black", 18), border=5)
-botonr.place(x = 555, y = 550) 
+botonr.place(x = 555, y = 500) 
 
 
 
-frame_logor = tk.Frame(ventanar, bd=0, width=300, relief=tk.SOLID, padx=0, pady=0, bg="black")
+frame_logor = tk.Frame(ventanar, bd=0, width=320, relief=tk.SOLID, padx=30, pady=210, bg="black")
 frame_logor.pack(side=tk.LEFT, expand=tk.NO, fill=tk.BOTH)
-labelr = tk.Label(frame_logor, image=iconor, bg="#F4F5F7")
-labelr.place(x=0, y=0, relwidth=1, relheight=1)
+labelr = tk.Label(frame_logor, image=iconor, bg="black")
+labelr.place(x=50, y=0, relwidth=1, relheight=1)
 
 
 
